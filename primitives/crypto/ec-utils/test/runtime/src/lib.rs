030f36f88dfe240b28f373e277876183f9852c41
Achim Schneider <achim@parity.io>
2023-06-13 12:50:29 +0200

    some unsatisfied trait bounds

--- primitives/crypto/ec-utils/test/runtime/src/lib.rs
+++ primitives/crypto/ec-utils/test/runtime/src/lib.rs
@@ -15,35 +15,122 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(any(feature = "std", test))]
+
+use sp_version::NativeVersion;
 use sp_api::{
 	decl_runtime_apis, impl_runtime_apis, mock_impl_runtime_apis, ApiError, ApiExt, ApiRef,
 	ConstructRuntimeApi, ProvideRuntimeApi, RuntimeApiInfo,
 };
-use sp_runtime::traits::Block as BlockT;
-#[cfg(any(feature = "std", test))]
-use sp_version::NativeVersion;
 use sp_version::{create_apis_vec, create_runtime_str, RuntimeVersion};
-
-use sp_core::Hash;
+use sp_core::{OpaqueMetadata, RuntimeDebug};
+use sp_core::hash::Hash;
+use sp_runtime::traits::Block as BlockT;
+use sp_runtime::generic::UncheckedExtrinsic;
 use sp_crypto_ec_utils::elliptic_curves;
-
+use codec::{Encode, Decode};
 use sp_consensus_grandpa::{
 	AuthorityList, EquivocationProof, GrandpaApi, OpaqueKeyOwnershipProof, GRANDPA_ENGINE_ID,
 };
-
 use frame_support::{
 	metadata_ir::{
 		RuntimeApiMetadataIR, RuntimeApiMethodMetadataIR, RuntimeApiMethodParamMetadataIR,
 	},
 	traits::ConstU32,
 };
+use frame_system::{CheckNonce, CheckWeight};
 use scale_info::{form::MetaForm, meta_type};
+use sp_runtime::traits::SignedExtension;
+use scale_info::TypeInfo;
 
-pub type BlockNumber = u64;
+pub mod substrate_test_pallet;
+
+#[cfg(feature = "std")]
+pub type Pair = sp_core::sr25519::Pair;
+
+pub type Address = sp_core::sr25519::Public;
+pub type BlockNumber = u32;
 pub type Index = u64;
-pub type Header = sp_runtime::generic::Header<u32, sp_runtime::traits::BlakeTwo256>;
-pub type Block = sp_runtime::generic::Block<Header, UncheckedExtrinsic>;
-pub type UncheckedExtrinsic = sp_runtime::generic::UncheckedExtrinsic<u32, RuntimeCall, (), ()>;
+pub type Signature = sr25519::Signature;
+
+#[derive(Copy, Clone, PartialEq, Eq, Encode, Decode, RuntimeDebug, TypeInfo)]
+pub struct CheckSubstrateCall;
+
+impl sp_runtime::traits::Printable for CheckSubstrateCall {
+	fn print(&self) {
+		"CheckSubstrateCall".print()
+	}
+}
+
+impl sp_runtime::traits::Dispatchable for CheckSubstrateCall {
+	type RuntimeOrigin = CheckSubstrateCall;
+	type Config = CheckSubstrateCall;
+	type Info = CheckSubstrateCall;
+	type PostInfo = CheckSubstrateCall;
+
+	fn dispatch(
+		self,
+		_origin: Self::RuntimeOrigin,
+	) -> sp_runtime::DispatchResultWithInfo<Self::PostInfo> {
+		panic!("This implementation should not be used for actual dispatch.");
+	}
+}
+
+/// Transfer data extracted from Extrinsic containing `Balances::transfer_allow_death`.
+#[derive(Clone, PartialEq, Eq, Encode, Decode, RuntimeDebug, TypeInfo)]
+pub struct TransferData {
+	pub from: AccountId,
+	pub to: AccountId,
+	pub amount: Balance,
+	pub nonce: Index,
+}
+
+impl sp_runtime::traits::SignedExtension for CheckSubstrateCall {
+	type AccountId = AccountId;
+	type Call = RuntimeCall;
+	type AdditionalSigned = ();
+	type Pre = ();
+	const IDENTIFIER: &'static str = "CheckSubstrateCall";
+
+	fn additional_signed(
+		&self,
+	) -> sp_std::result::Result<Self::AdditionalSigned, TransactionValidityError> {
+		Ok(())
+	}
+
+	fn validate(
+		&self,
+		_who: &Self::AccountId,
+		call: &Self::Call,
+		_info: &DispatchInfoOf<Self::Call>,
+		_len: usize,
+	) -> TransactionValidity {
+		log::trace!(target: LOG_TARGET, "validate");
+		match call {
+			RuntimeCall::SubstrateTest(ref substrate_test_call) =>
+				substrate_test_pallet::validate_runtime_call(substrate_test_call),
+			_ => Ok(Default::default()),
+		}
+	}
+
+	fn pre_dispatch(
+		self,
+		who: &Self::AccountId,
+		call: &Self::Call,
+		info: &sp_runtime::traits::DispatchInfoOf<Self::Call>,
+		len: usize,
+	) -> Result<Self::Pre, TransactionValidityError> {
+		self.validate(who, call, info, len).map(drop)
+	}
+}
+
+pub type SignedExtra = (CheckNonce<Runtime>, CheckWeight<Runtime>, CheckSubstrateCall);
+pub type SignedPayload = sp_runtime::generic::SignedPayload<RuntimeCall, SignedExtra>;
+pub type Header = sp_runtime::generic::Header<BlockNumber, sp_runtime::traits::BlakeTwo256>;
+pub type Extrinsic = sp_runtime::generic::UncheckedExtrinsic<Address, RuntimeCall, Signature, SignedExtra>;
+pub type Block = sp_runtime::generic::Block<Header, Extrinsic>;
 
 impl frame_system::Config for Runtime {
 	type BlockWeights = ();
@@ -76,9 +163,10 @@ frame_support::construct_runtime!(
 	pub enum Runtime where
 		Block = Block,
 		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic
+		UncheckedExtrinsic = Extrinsic
 	{
 		System: frame_system,
+		SubstrateTest: substrate_test_pallet::pallet,
 	}
 );
 
@@ -89,7 +177,7 @@ decl_runtime_apis! {
 	}
 }
 
-impl_runtime_apis! {
+mock_impl_runtime_apis! {
 	impl self::Api<Block> for Runtime {
 		fn test_bls12_381_g1_mul_projective_crypto(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8> {
 			let result = sp_crypto_ec_utils::elliptic_curves::bls12_381_mul_projective_g1(base, scalar)
