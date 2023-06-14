69fdde8934019660e0914b8179134257cfcb172c
Achim Schneider <achim@parity.io>
2023-06-14 19:07:53 +0200

    api error handling without runtime debug

--- primitives/crypto/ec-utils/test/runtime/src/errors.rs
+++ primitives/crypto/ec-utils/test/runtime/src/errors.rs
@@ -1,15 +1,11 @@
+use codec::{Decode, Encode};
 use scale_info::TypeInfo;
-use sp_runtime::{
-	codec::{Decode, Encode},
-	RuntimeDebug,
-};
+use sp_std::fmt::Debug;
 
 /// An error describing which elliptic curve call failed.
-#[derive(Clone, Copy, PartialEq, Eq, Encode, Decode, RuntimeDebug, TypeInfo)]
+#[derive(Clone, Copy, PartialEq, Eq, Encode, Decode, TypeInfo, Debug)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum EccError {
 	// #[error("Failed to compute projective mul for g1 on bls12_381")]
 	Bls12_381G1Projective,
 }
-
-// impl sp_blockchain::Error for EccError {}
